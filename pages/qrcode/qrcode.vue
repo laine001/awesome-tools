bbcf120756e1313bc2cb2ffe8b9dd7d0cc344560
LAINE001 <laine001@163.com>
2025-06-01 20:44:41 +0800

    🚀

--- pages/qrcode/qrcode.vue
+++ pages/qrcode/qrcode.vue
@@ -1,12 +1,10 @@
 <template>
 	<view>
 		123
-		<uv-qrcode ref="qrcode" size="300px" value="https://h5.uvui.cn"></uv-qrcode>
 	</view>
 </template>
 
 <script setup>
-	import UvQrcode from '@/uni_modules/uv-qrcode/components/uv-qrcode/uv-qrcode.vue'
 </script>
 
 <style>
